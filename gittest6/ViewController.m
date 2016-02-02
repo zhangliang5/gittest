058441a56255848acdfe8557cc516992cfb1122b
zhangliang <zhangliang5@126.com>
2016-02-02 10:27:08 +0800

    1

--- gittest6/ViewController.m
+++ gittest6/ViewController.m
@@ -18,6 +18,10 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     NSLog(@"hello world");
+    NSLog(@"hello world");
+    NSLog(@"hello world");
+    
+    NSLog(@"hello world");
 }
 
 - (void)didReceiveMemoryWarning {
