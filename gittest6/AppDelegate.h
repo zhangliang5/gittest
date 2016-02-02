e2fda49d400237e4a810c33a2f89f120ab0a60dd
zhangliang <zhangliang5@126.com>
2016-02-02 08:45:38 +0800

    Initial Commit

---
+++ gittest6/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gittest6
+//
+//  Created by zhangliang on 16/2/2.
+//  Copyright © 2016年 com.crscic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gittest6/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gittest6
+//
+//  Created by zhangliang on 16/2/2.
+//  Copyright © 2016年 com.crscic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
